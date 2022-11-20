ff8fce3e317b41ec89b5dc0e23e61d402364368a
vicente_nascimento <vicente19981@live.com>
2022-11-20 14:21:19 -0300

    feat: initialize server of graphql and database on sqlite3

---
+++ cmd/server/server.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/vicentenascimento/graphql/graph"
+	"github.com/vicentenascimento/graphql/graph/generated"
+	"github.com/vicentenascimento/graphql/internal/database"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatalf("Failed to open database: %v", err)
+	}
+
+	defer db.Close()
+
+	categoryDb := database.NewCategory(db)
+	courseDb := database.NewCourse(db)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{
+		CategoryDB: categoryDb,
+		CourseDB:   courseDb,
+	}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
